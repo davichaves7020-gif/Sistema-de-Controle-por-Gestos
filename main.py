269e6a8febe7af54adc28932d1b67ef7eadd2f1c
davichaves7020 <davichaves7020@gmail.com>
2026-09-18 15:51:50 -0300

    Adicionado a função de clique duplo

--- main.py
+++ main.py
@@ -37,6 +37,7 @@ class ControladorNovaAPI:
         # Estados dos botões para lógica de gatilho rápido (evita clique infinito)
         self.clique_esquerdo_pressionado = False
         self.clique_direito_pressionado = False
+        self.clique_duplo_pressionado = False
         
         # Pontos do Olho Direito para cálculo do EAR matemático
         self.PALPEBRA_SUP_DIREITA = 159
@@ -84,69 +85,90 @@ class ControladorNovaAPI:
         )
         self.detector_rosto = vision.FaceLandmarker.create_from_options(opcoes_rosto)
 
-    def processar_mouse(self, img_w, img_h):
-        """
-        Move o mouse com o indicador.
-        Dedo Médio dobrado = Clique Esquerdo (Gatilho rápido)
-        Dedo Indicador dobrado = Clique Direito (Gatilho rápido)
-        """
-        if not self.ultimos_resultados_mao or not self.ultimos_resultados_mao.hand_landmarks:
-            return "NENHUM", None
-        
-        landmarks_lista = self.ultimos_resultados_mao.hand_landmarks
-        if len(landmarks_lista) == 0:
-            return "NENHUM", None
+        def processar_mouse(self, img_w, img_h):
+            """
+            Move o mouse com o indicador.
+            Indicador e Médio dobrados juntos = Clique Duplo (Gatilho)
+            Apenas Dedo Médio dobrado = Clique Esquerdo (Gatilho)
+            Apenas Dedo Indicador dobrado = Clique Direito (Gatilho)
+            """
+            if not self.ultimos_resultados_mao or not self.ultimos_resultados_mao.hand_landmarks:
+                return "NENHUM", None
             
-        landmarks = landmarks_lista[0] # Acessa a primeira mão detectada
+            landmarks_lista = self.ultimos_resultados_mao.hand_landmarks
+            if len(landmarks_lista) == 0:
+                return "NENHUM", None
+                
+            landmarks = landmarks_lista[0] # Acessa a primeira mão detectada
+            
+            p_indicador_ponta = landmarks[self.DEDO_INDICADOR] # Ponto 8 (Ponta)
+            p_indicador_meio = landmarks[6]                   # Ponto 6 (Articulação)
+            
+            p_medio_ponta = landmarks[self.DEDO_MEDIO]         # Ponto 12 (Ponta)
+            p_medio_meio = landmarks[10]                       # Ponto 10 (Articulação)
+
+            # 1. Movimentação do Cursor (Continua seguindo a ponta do indicador)
+            margem = 0.15
+            na_tela_x = np.interp(p_indicador_ponta.x, [margem, 1.0 - margem], [0, self.largura_tela])
+            na_tela_y = np.interp(p_indicador_ponta.y, [margem, 1.0 - margem], [0, self.altura_tela])
+
+            # Filtro de Média Móvel para Suavização
+            self.historico_x.append(na_tela_x)
+            self.historico_y.append(na_tela_y)
+            if len(self.historico_x) > self.suavizacao:
+                self.historico_x.pop(0)
+                self.historico_y.pop(0)
+            
+            mouse_x = int(np.mean(self.historico_x))
+            mouse_y = int(np.mean(self.historico_y))
+            pyautogui.moveTo(mouse_x, mouse_y)
+
+            # Coordenadas em pixels do indicador para retorno visual
+            x1, y1 = int(p_indicador_ponta.x * img_w), int(p_indicador_ponta.y * img_h)
+            
+            estado_clique = "NENHUM"
+
+            # Identifica se os dedos estão dobrados (Ponta abaixo da articulação do meio)
+            indicador_dobrado = p_indicador_ponta.y > p_indicador_meio.y
+            medio_dobrado = p_medio_ponta.y > p_medio_meio.y
+
+            # 2. LÓGICA DOS GATILHOS COM PRIORIDADE
+            if indicador_dobrado and medio_dobrado:
+                # Ambos dobrados: CLIQUE DUPLO
+                if not self.clique_duplo_pressionado:
+                    pyautogui.doubleClick()
+                    self.clique_duplo_pressionado = True
+                estado_clique = "DUPLO"
+                
+                # Reseta os outros gatilhos para não causarem conflito
+                self.clique_esquerdo_pressionado = False
+                self.clique_direito_pressionado = False
+                
+            elif medio_dobrado:
+                # Apenas o Médio: CLIQUE ESQUERDO
+                if not self.clique_esquerdo_pressionado:
+                    pyautogui.click(button='left')
+                    self.clique_esquerdo_pressionado = True
+                estado_clique = "ESQUERDO"
+                self.clique_duplo_pressionado = False
+                
+            elif indicador_dobrado:
+                # Apenas o Indicador: CLIQUE DIREITO
+                if not self.clique_direito_pressionado:
+                    pyautogui.click(button='right')
+                    self.clique_direito_pressionado = True
+                estado_clique = "DIREITO"
+                self.clique_duplo_pressionado = False
+                
+            else:
+                # Nenhum dedo dobrado: Libera todos os gatilhos para o próximo clique
+                self.clique_esquerdo_pressionado = False
+                self.clique_direito_pressionado = False
+                self.clique_duplo_pressionado = False
+            
+            return estado_clique, (x1, y1)
+
         
-        p_indicador_ponta = landmarks[self.DEDO_INDICADOR] # Ponto 8 (Ponta)
-        p_indicador_meio = landmarks[6]                   # Ponto 6 (Articulação)
-        
-        p_medio_ponta = landmarks[self.DEDO_MEDIO]         # Ponto 12 (Ponta)
-        p_medio_meio = landmarks[10]                      # Ponto 10 (Articulação)
-
-        # 1. Movimentação do Cursor (Continua seguindo a ponta do indicador)
-        margem = 0.15
-        na_tela_x = np.interp(p_indicador_ponta.x, [margem, 1.0 - margem], [0, self.largura_tela])
-        na_tela_y = np.interp(p_indicador_ponta.y, [margem, 1.0 - margem], [0, self.altura_tela])
-
-        # Filtro de Média Móvel para Suavização
-        self.historico_x.append(na_tela_x)
-        self.historico_y.append(na_tela_y)
-        if len(self.historico_x) > self.suavizacao:
-            self.historico_x.pop(0)
-            self.historico_y.pop(0)
-        
-        mouse_x = int(np.mean(self.historico_x))
-        mouse_y = int(np.mean(self.historico_y))
-        pyautogui.moveTo(mouse_x, mouse_y)
-
-        # Coordenadas em pixels do indicador para retorno visual
-        x1, y1 = int(p_indicador_ponta.x * img_w), int(p_indicador_ponta.y * img_h)
-        
-        estado_clique = "NENHUM"
-
-        # 2. LOGICA DO CLIQUE ESQUERDO: Dedo Médio dobrado (Gatilho)
-        if p_medio_ponta.y > p_medio_meio.y:
-            if not self.clique_esquerdo_pressionado:
-                pyautogui.click(button='left')
-                self.clique_esquerdo_pressionado = True # Trava para não repetir
-            estado_clique = "ESQUERDO"
-        else:
-            self.clique_esquerdo_pressionado = False # Libera o gatilho quando esticar o dedo
-
-        # 3. LÓGICA DO CLIQUE DIREITO: Dedo Indicador dobrado (Gatilho)
-        if p_indicador_ponta.y > p_indicador_meio.y:
-            if not self.clique_direito_pressionado:
-                pyautogui.click(button='right')
-                self.clique_direito_pressionado = True # Trava para não repetir
-            estado_clique = "DIREITO"
-        else:
-            self.clique_direito_pressionado = False # Libera o gatilho quando esticar o dedo
-        
-        return estado_clique, (x1, y1)
-
-    
     def processar_piscada(self, frame, img_w, img_h):
         """Detecta a proporção geométrica (EAR) da piscada do olho direito."""
         if not self.ultimos_resultados_rosto or not self.ultimos_resultados_rosto.face_landmarks:
@@ -232,6 +254,21 @@ def iniciar(self):
 
             # Fluxo de processamento condicional
             if not sistema_pausado:
+                estado_clique, coord_mao = self.processar_mouse(img_w, img_h)
+                self.processar_piscada(frame, img_w, img_h)
+                
+                if coord_mao:
+                    # Define as cores do cursor para feedback visual rápido
+                    if estado_clique == "ESQUERDO":
+                        cor_cursor = (0, 0, 255)  # Vermelho
+                    elif estado_clique == "DIREITO":
+                        cor_cursor = (255, 0, 0)  # Azul
+                    elif estado_clique == "DUPLO":
+                        cor_cursor = (255, 0, 255) # Rosa / Magenta
+                    else:
+                        cor_cursor = (0, 255, 0)  # Verde (Livre)
+                        
+                    cv2.circle(frame, coord_mao, 8, cor_cursor, -1)
                 estado_clique, coord_mao = self.processar_mouse(img_w, img_h)
                 self.processar_piscada(frame, img_w, img_h)
                 
